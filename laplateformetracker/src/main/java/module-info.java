c4f3212857a35731dcbdb104f1469d4737880abc
arthur-georget <arthur.georget@laplateforme.io>
2026-04-08 11:43:49 +0200

    feat(modules): pgJDBG and junit dependencies added. jacoco and surefire(tests) plugins added.

--- laplateformetracker/src/main/java/module-info.java
+++ laplateformetracker/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module laplateformetracker {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
     opens laplateformetracker to javafx.fxml;
     exports laplateformetracker;
