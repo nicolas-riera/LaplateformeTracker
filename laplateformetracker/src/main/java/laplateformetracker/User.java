03b8207f6b5a5b7135b826b90394545c015c2ebf
arthur-georget <arthur.georget@laplateforme.io>
2026-04-10 14:16:34 +0200

    feat(User): new class User done.

--- laplateformetracker/src/main/java/laplateformetracker/User.java
+++ laplateformetracker/src/main/java/laplateformetracker/User.java
@@ -1,5 +1,41 @@
 package laplateformetracker;
 
 public class User {
+    private String id;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private Boolean isManager;
 
+    public User(String id, 
+                String email,
+                String firstName,
+                String lastName,
+                Boolean isManager
+                ){
+                    this.id = id;
+                    this.email = email;
+                    this.firstName = firstName;
+                    this.lastName = lastName;
+                }
+
+    public String getId(){
+        return this.id;
+    }
+
+    public String getEmail(){
+        return this.email;
+    }
+
+    public String getFirstName(){
+        return this.firstName;
+    }
+
+    public String getLastName(){
+        return this.lastName;
+    }
+
+    public Boolean getIsManager(){
+        return this.isManager;
+    }
 }
