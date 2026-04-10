fa4448d0067818fa48fffd20d3a4033507b09aef
nicolas-riera <nicolas.riera@laplateforme.io>
2026-04-10 14:36:54 +0200

    feat: added login checks

--- laplateformetracker/src/main/java/laplateformetracker/controllers/login/LoginController.java
+++ laplateformetracker/src/main/java/laplateformetracker/controllers/login/LoginController.java
@@ -4,21 +4,25 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import laplateformetracker.models.DataBase;
+import laplateformetracker.models.ManagerModel;
+import laplateformetracker.models.StudentModel;
 import laplateformetracker.views.LoginView;
+
+import java.util.ArrayList;
 
 import at.favre.lib.crypto.bcrypt.BCrypt;
 
 public class LoginController {
     private LoginView loginview;
+    private DataBase database;
 
     public LoginController(Stage stage) throws java.io.IOException {
 
-        DataBase database = new DataBase();
+         this.database = new DataBase();
 
         this.loginview = new LoginView(stage);
         this.loginview.getFxmlController().setOnLoginButtonCallback((user, pass) -> {
-            System.out.println(user);
-            System.out.println(hashPassword(pass));
+
         }
         );
         Rectangle r = new Rectangle(25,25,250,250);
@@ -27,12 +31,41 @@ public class LoginController {
         
     }
 
-    public String hashPassword(String password) {
-        return BCrypt.withDefaults().hashToString(12, password.toCharArray());
+    // public String hashPassword(String password) {
+    //     return BCrypt.withDefaults().hashToString(12, password.toCharArray());
+    // }
+
+    public Boolean checkPassword(String password, String stored_password) {
+        if (password.isEmpty()) {
+            return false;
+        } else {
+            BCrypt.Result result = BCrypt.verifyer().verify(password.toCharArray(), stored_password);
+            return result.verified;
+        }
     }
 
-    public Boolean checkPassword(String email, String hashed_password) {
-        String stored_password;
-        return false;
+    public void loginUser(String email, String password) {
+        Integer manager_id = ManagerModel.getID(email, database);
+        Integer student_id = StudentModel.getID(email, database);
+
+        if (manager_id != -1) {
+            ArrayList<ArrayList<String>> user_infos = ManagerModel.getInfos(manager_id, database);
+            if (user_infos.get(0).get(2) == null || user_infos.get(0).get(2).isEmpty()) {
+                // Process password creation
+            }  else if (checkPassword(password, user_infos.get(0).get(2))) {
+                // Instantiate Manager
+            } else {
+                // Message "Email ou mot de passe incorrect."
+            }
+        } else if (student_id != -1) {
+            ArrayList<ArrayList<String>> user_infos = StudentModel.getInfos(student_id, database);
+            if (checkPassword(password, user_infos.get(0).get(2))) {
+                // Instantiate Student ?
+            } else {
+                // Message "Email ou mot de passe incorrect."
+            }
+        } else {
+            // Message "Email ou mot de passe incorrect."
+        }
     }
 }
