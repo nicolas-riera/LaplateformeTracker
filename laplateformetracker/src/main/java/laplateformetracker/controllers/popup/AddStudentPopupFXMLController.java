110b35ef35cc5e7ad55377aea70a023d5a140f00
nicolas-riera <nicolas.riera@laplateforme.io>
2026-04-14 15:29:59 +0200

    add(exp): added fmxl elements to backend

--- laplateformetracker/src/main/java/laplateformetracker/controllers/popup/AddStudentPopupFXMLController.java
+++ laplateformetracker/src/main/java/laplateformetracker/controllers/popup/AddStudentPopupFXMLController.java
@@ -3,12 +3,35 @@ package laplateformetracker.controllers.popup;
 import java.util.ArrayList;
 import java.util.function.Consumer;
 
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 
 public class AddStudentPopupFXMLController {
+
+    @FXML
+    private TextField lastNameField;
+
+    @FXML
+    private TextField firstNameField;
+
+    @FXML
+    private DatePicker birthDatePicker;
+
+    @FXML
+    private TextField addressField;
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private TextField phoneField;
+
+    @FXML
+    private ChoiceBox<String> DegreePicker;
     
     @FXML
     private Button addButton;
